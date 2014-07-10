96172f01e441e5db4c0b4d37de57759f54ac97f9
Vincenzo Maffione <v.maffione@gmail.com>
2014-07-10 22:25:27 +0200

    proxy merged into manager

--- src/manager.cpp
+++ src/manager.cpp
@@ -11,6 +11,8 @@
 #include <unistd.h>
 #include <string>
 #include <list>
+#include <signal.h>
+#include <cerrno>
 
 using namespace std;
 
@@ -199,16 +201,152 @@ int ManagerServer::process_request(RemoteConnection& connection)
             content = "No previous join";
         }
         Response(content).serialize(connection);
+    } else if (opcode == UPDATE) {
+        UpdateRequest request;
+
+        request.deserialize(connection);
+        cout << "UPDATE-REQUEST-" << request.add << "-(" <<
+                request.members.size() << ")" << endl;
+        for (unsigned int i = 0; i < request.members.size(); i++) {
+            const Member& m = request.members[i];
+            cout << "   Member " << m.ip << " " << m.port << " "
+                    << m.id << " " << m.color << endl;
+        }
     }
 
     return 0;
 }
 
-int main()
+/* Set to "true" by join() if the operation is successful. */
+static bool joined = false;
+static unsigned int server_port = ~0U;
+static unsigned int join_port = ~0U;
+
+static int
+join()
 {
-    ManagerServer server(MANAGER_PORT);
+    Remote remote("127.0.0.1", join_port);
+    RemoteConnection connection(remote);
+    JoinRequest message("127.0.0.1", server_port);
+    Response response;
+
+    message.serialize(connection);
+
+    response.deserialize(connection);
+
+    connection.close();
+
+    if (response.content != "OK") {
+        cout << "Join error: " << response.content << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    joined = true;
+
+    return 0;
+}
+
+static int
+leave()
+{
+    Remote remote("127.0.0.1", join_port);
+    RemoteConnection connection(remote);
+    LeaveRequest request("127.0.0.1", server_port);
+    Response response;
+
+    request.serialize(connection);
+    response.deserialize(connection);
+    connection.close();
+
+    if (response.content != "OK") {
+        cout << "Leave error: " << response.content << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    joined = false;
+
+    return 0;
+}
+
+struct ServerArgs {
+    unsigned int port;
+};
+
+static void *
+server(void *arg)
+{
+    ServerArgs *sargs = static_cast<ServerArgs*>(arg);
+    ManagerServer server(sargs->port);
 
     server.run();
+
+    return NULL;
+}
+
+static void
+sigint_handler(int signum)
+{
+    if (joined) {
+        leave();
+    }
+
+    exit(EXIT_SUCCESS);
+}
+
+int
+main(int argc, char **argv)
+{
+    unsigned int s_port;
+    unsigned int j_port = ~0U;
+    struct sigaction sa;
+    pthread_t server_tid;
+    ServerArgs sargs;
+
+    if (argc < 2) {
+        exit_with_error("USAGE: program PORT [JOINPORT]");
+    }
+    s_port = atoi(argv[1]);
+    if (s_port >= 65535) {
+        errno = EINVAL;
+        exit_with_error("PORT > 65535");
+    }
+    server_port = s_port;
+
+    if (argc > 2) {
+        j_port = atoi(argv[2]);
+        if (j_port >= 65535) {
+            errno = EINVAL;
+            exit_with_error("PORT > 65535");
+        }
+        join_port = j_port;
+    }
+
+    sa.sa_handler = sigint_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    if (sigaction(SIGINT, &sa, NULL) == -1) {
+        cout << "   Warning: SIGINT handler registration failed" << endl;
+    }
+    if (sigaction(SIGTERM, &sa, NULL) == -1) {
+        cout << "   Warning: SIGTERM handler registration failed" << endl;
+    }
+
+    /* Start the server. */
+    sargs.port = s_port;
+    if (pthread_create(&server_tid, NULL, server, (void *)&sargs)) {
+        exit_with_error("pthread_create()");
+    }
+
+    if (j_port != ~0U) {
+        /* Carry out the join procedure with the manager server. */
+        join();
+    }
+
+    /* Wait for the member server to complete - it still holds the memory
+     * for 'sargs', which is ours. */
+    if (pthread_join(server_tid, NULL)) {
+        exit_with_error("pthread_join()");
+    }
 
     return 0;
 }
