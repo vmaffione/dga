3f75c4f0f88f91a8252857b7b2d7a04ce08b5ac7
Vincenzo Maffione <v.maffione@gmail.com>
2014-07-25 22:27:48 +0200

    better main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,11 @@ using namespace std;
 /* FloatVector example */
 float objfcn(const FloatVector& v)
 {
-    return 3*(v[0])*(v[0]) + 5*(v[1])*(v[1]);
+    if (v[0] > 5.0 && v[1] > 5.0) {
+        return 5*(v[0] - 14.23)*(v[0] - 14.23) + 3*(v[1] - 31.98)*(v[1] - 31.98);
+    }
+
+    return 12*(v[0] - 1.3)*(v[0] - 1.3) + 14*(v[1] + 3.14)*(v[1] + 3.14);
 }
 
 ///////////////////////
@@ -35,24 +39,23 @@ int work(GAPeerServer *serv)
         unsigned int numIter = 750;
         unsigned int elichi = (((unsigned int)(popsize*0.04)) > 1) ? ((unsigned int)(popsize*0.04)) : 1;  // number of elite children
 
-        /*
-           cout << "\n\n FLOATVECTOR EXAMPLE: \n";
-           vector<float> lbs(2);  // lower bounds
-           lbs[0] = -5.0; lbs[1] = -10;
-           vector<float> ubs(2);   // upper bounds
-           ubs[0] = 6.0; ubs[1] = 6.5;
+#if 0
+        /* Float vector example. */
+        vector<float> lbs(2);  // lower bounds
+        lbs[0] = -100.0; lbs[1] = -100.0;
+        vector<float> ubs(2);   // upper bounds
+        ubs[0] = 100.0; ubs[1] = 100.0;
 
-           GeneticAlgorithm<FloatVector, float> G(&objfcn, GAUtils::MutationGaussian, GAUtils::CrossoverScattered, GAUtils::SUS);
-           vector<FloatVector> inipop;
-           G.gaUtils.generate(popsize, inipop, lbs, ubs);  // generates random initial population
-           G.run(inipop, numIter, 0.8, elichi);              // run the algorithm
-           */
-
-        //GeneticAlgorithm<float, float> G2(&obj, &mt, &cr, GAUtils::SUS);
+        GeneticAlgorithm<FloatVector, float> G(&objfcn, GAUtils::MutationGaussian, GAUtils::CrossoverScattered, GAUtils::SUS, *serv);
+        vector<FloatVector> inipop;
+        G.gaUtils.generate(popsize, inipop, lbs, ubs);  // generates random initial population
+        G.run(inipop, numIter, 0.8, elichi);              // run the algorithm
+#else
         GeneticAlgorithm<float, float> G2(&obj, GAUtils::MutationGaussian, GAUtils::CrossoverConvex, GAUtils::SUS, *serv);
         vector<float> finipop;
         G2.gaUtils.generate(popsize, finipop, -10.0, 13.3);
         G2.run(finipop, numIter, 0.8, elichi);
+#endif
     }
     catch (GAError) {  }
 
